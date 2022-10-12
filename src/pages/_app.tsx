68fa3da3a87161ea442e12dc5be73cdec80d4672
Cleyton Furtado <cleytonrodrigues69@live.com>
2022-10-12 18:28:42 -0300

    feat: Initial config

---
+++ src/pages/_app.tsx
@@ -0,0 +1,21 @@
+import type { AppProps } from 'next/app'
+import Head from 'next/head'
+import GlobalStyles from 'styles/global'
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <title>React Avançado - Boilerplate</title>
+        <link rel="shortcut icon" href="/img/favicon.ico" />
+        <link rel="apple-touch-icon" href="/img/favicon.ico" />
+        <meta
+          name="description"
+          content="A simnple project stater to work with Typéscript, React, NextJs and Styles components"
+        />
+      </Head>
+      <GlobalStyles />
+      <Component {...pageProps} />
+    </>
+  )
+}
